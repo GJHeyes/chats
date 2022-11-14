4d8a639d3f3419c8651b05d0735230d4d6855451
GJHeyes <gregory-heyes@hotmail.com>
2022-11-14 18:53:14 +0000

    first commit

---
+++ public/main.js
@@ -0,0 +1,131 @@
+const ws = new WebSocket('ws://localhost:3000/chats')
+
+const chatForm = document.getElementById('chat')
+const otherChats = document.getElementById('chats')
+//const myChats = document.getElementById('myChats')
+const userForm = document.getElementById('user')
+const userMain = document.getElementById('userName')
+const main = document.getElementById('main')
+
+chatForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const chat = formData.get('chat')
+    const user = JSON.parse(localStorage.getItem("user"))
+    const body = JSON.stringify({chat, status: 0,UserId: user.id})
+    fetch('/chats', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: body
+    })
+    .then(res => res.json())
+    .then(chat => {
+        event.target.reset()
+    })
+    ws.send(body)
+    event.target.reset()
+})
+
+ws.addEventListener('message', chat => {
+    const el = document.createElement('li')
+    const chatData = JSON.parse(chat.data)
+    el.innerHTML = chatData.chat
+    console.log(chatData.UserId)
+    const localUserId = (JSON.parse(localStorage.getItem('user'))).id
+    if(chatData.UserId === localUserId){
+        el.classList = ('myChats chatting')
+        //myChats.appendChild(el)
+        
+    }else{
+        el.classList = ('otherChats chatting')
+        //otherChats.appendChild(el)
+    }
+    otherChats.appendChild(el)
+})
+
+userForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const username = formData.get('user').toString()
+    let userID = undefined
+    try{
+        localUserId = (JSON.parse(localStorage.getItem('user'))).id
+    }catch(error){
+        localUserId = ""
+    }
+    fetch('/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({username, status: 0,userID: userID})
+    })
+    .then(res => res.json())
+    .then(user =>{
+        localStorage.setItem("user", JSON.stringify(user))
+    })
+    userForm.classList.add('hidden')
+    userMain.classList.add('hidden')
+    main.classList.remove('hidden')
+})
+
+function renderChats () {
+   // myChats.innerHTML = ""
+    otherChats.innerHTML = ""
+    fetch('/chats')
+    .then(res => res.json())
+    .then(users => {
+        console.log(users)
+        users.forEach(chats => {
+            const li = document.createElement('li')
+            li.innerHTML = `${chats.chat}`
+            let localUserId 
+            try{
+                localUserId = (JSON.parse(localStorage.getItem('user'))).id
+            }catch(error){
+                localUserId = ""
+            }
+            if(chats.UserId === localUserId){
+                li.classList = ('myChats chatting')
+                //myChats.appendChild(li)
+            }else{
+                li.classList = ('otherChats chatting')
+                //
+            }
+            otherChats.appendChild(li)
+        })
+    })
+    .catch(console.error)
+}
+
+function getUser(){
+    const user = localStorage.getItem("user")
+    if(user){
+        const myUser = JSON.parse(user)
+        const input = document.getElementById("user-input")
+        input.value = myUser.username
+        userForm.classList.add('hidden')
+        userMain.classList.add('hidden')
+        main.classList.remove('hidden')
+    }
+}
+
+getUser()
+
+renderChats()
+
+// chatForm.addEventListener('submit', function (event) {
+//     event.preventDefault()
+//     const formData = new FormData(event.target)
+//     const msg = formData.get('chat')
+//     ws.send(msg)
+//     event.target.reset()
+// })
+
+// ws.addEventListener('message', msg => {
+//     const el = document.createElement('li')
+//     el.innerHTML = msg.data
+//     chats.appendChild(el)
+// })
---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const app = express()
+const expressWs = require('express-ws')(app)
+
+const { Sequelize, Model, DataTypes } = require('sequelize')
+const sequelize = new Sequelize('sqlite:chats.sqlite')
+
+class Chat extends Model {}
+class User extends Model {}
+Chat.init({
+    chat: DataTypes.STRING,
+    status: DataTypes.BOOLEAN,
+}, { sequelize })
+
+User.init({
+    username: DataTypes.STRING,
+    status: DataTypes.BOOLEAN
+}, { sequelize })
+
+Chat.belongsTo(User)
+User.hasMany(Chat)
+
+app.use(express.json())
+app.use(express.static('public'))
+
+app.get('/chats', async (req, res) => {
+    const chats = await Chat.findAll({include: {model: User, required:true}})
+    res.send(chats)
+})
+
+app.post('/chats', async (req, res) => {
+    console.log(req.body)
+    const user = await User.findByPk(req.body.UserId)
+    const chat = await Chat.create(req.body)
+    await user.addChat(chat)
+    //const chat = await user.addChat(req.body.chat)
+    res.send(chat)
+    console.log(req.body)
+    // . const user = await User.findByPk(req.body.UserId)
+    
+    //
+   // const addchat = await user.addChat(req.body.chat)
+    
+    //res.send(addchat)
+})
+
+app.ws('/chats', (ws, req) => {
+    ws.on('message', chat => {
+        expressWs.getWss().clients.forEach(client => {
+            client.send(chat)
+        })
+    })
+})
+
+app.post('/users', async (req, res) => {
+    const previousUser = await User.findByPk(req.body.userID)
+    if(previousUser){
+        previousUser.update({username: req.body.username})
+        res.send(previousUser)
+    }else{
+        const user = await User.create(req.body)
+        res.send(user)
+    }
+    
+})
+
+app.get('/users', async (req, res) => {
+    const users = await User.findAll()
+    res.send(users)
+})
+
+app.listen(3000, async () => {
+    await sequelize.sync()
+    console.log('ready for chats...')
+})
