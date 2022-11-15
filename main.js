0df610cf2db1181d2f68e5901b6515315e4e5339
GJHeyes <gregory-heyes@hotmail.com>
2022-11-15 18:44:40 +0000

    changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const wsUrl = window.location.origin.includes('localhost') ? "ws://localhost:3000/chats" : "wss://gjheyes.github.io/chats/"
-const ws = new WebSocket("wss://newmessenger.onrender.com"),
+const ws = new WebSocket("wsUrl"),
     chatForm = document.getElementById('chat'),
     otherChats = document.getElementById('chats'),
     userForm = document.getElementById('user'),
