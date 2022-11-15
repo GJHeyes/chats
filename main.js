149522844c650feb4ed223c546a373089fa53a7e
GJHeyes <gregory-heyes@hotmail.com>
2022-11-15 19:02:14 +0000

    changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const wsUrl = window.location.origin.includes('localhost') ? "ws://localhost:3000/chats" : "wss://gjheyes.github.io/chats/"
-const ws = new WebSocket("wsUrl"),
+const ws = new WebSocket(wsUrl),
     chatForm = document.getElementById('chat'),
     otherChats = document.getElementById('chats'),
     userForm = document.getElementById('user'),
